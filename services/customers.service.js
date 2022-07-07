7d98a68d370e8ce0b7a3cfd8deec3fb7f424098a
jcasadiego <jcasadiego.a@gmail.com>
2022-07-07 07:15:23 -0400

    implementando bcrypt para crear un user

--- services/customers.service.js
+++ services/customers.service.js
@@ -1,5 +1,6 @@
 const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
+const bcrypt = require('bcrypt');
 
 class CustomerService {
 
@@ -21,9 +22,18 @@ class CustomerService {
   }
 
   async create(data) {
-    const newCustomer = await models.Customer.create(data, {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    const newData = {
+      ...data,
+      user: {
+        ...data.user,
+        password: hash
+      }
+    }
+    const newCustomer = await models.Customer.create(newData, {
       include: ['user']
     });
+    delete newCustomer.user.dataValues.password;
     return newCustomer;
   }
 
